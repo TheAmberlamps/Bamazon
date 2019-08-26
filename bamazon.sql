d94e0d62ccaacfa2130e9b1ac07c29ca1fdb82ef
Paul Unsted <ed__84@hotmail.com>
2019-08-26 06:42:39 -0400

    Forgot to add a .sql schema that was necessary for this app to function properly. Hopefully now my project can be graded properly.

---
+++ bamazon.sql
@@ -0,0 +1,44 @@
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products(
+    id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR (50) NOT NULL,
+    department_name VARCHAR (50) NOT NULL,
+    price INT NOT NULL,
+    stock_quantity INT NOT NULL
+    PRIMARY KEY (id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("laptopComputer", "electronics", 750, 12);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("bedsideLamp", "lighting", 22, 64);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("wok", "cooking", 63, 46);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("cellphone", "electronics", 315, 27);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("hdmiCable", "electronics", 20, 38);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("ledBulb", "lighting", 14, 49);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("whisk", "cooking", 7, 33);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("pressureCooker", "cooking", 155, 7);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("gardenHose", "lawn&garden", 37, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("pressureWasher", "lawn&garden", 331, 18);
